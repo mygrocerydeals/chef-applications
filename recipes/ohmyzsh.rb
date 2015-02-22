ddefa708244a1252b5f6be0eab82f00f43a7d784
Daryl Robbins <drobbins@mygrocerydeals.com>
2015-02-22 12:43:41 -0500

    Fix ohmyzsh script perms

--- recipes/ohmyzsh.rb
+++ recipes/ohmyzsh.rb
@@ -4,6 +4,7 @@ home = node['etc']['passwd'][node['current_user']]['dir']
 remote_file "#{Chef::Config[:file_cache_path]}/ohmyzsh_install.sh" do
     source 'https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh'
     owner node['current_user']
+    mode '0755'
     action :create_if_missing
 end
 

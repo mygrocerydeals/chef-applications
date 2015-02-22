69adb3e533685aaec088c8e801b1df9e37d20e46
Daryl Robbins <drobbins@mygrocerydeals.com>
2015-02-22 12:22:06 -0500

    APM: Improve execute description

--- providers/atom_package.rb
+++ providers/atom_package.rb
@@ -3,7 +3,7 @@ use_inline_resources
 
 action :install do
 
-  execute "Install APM Package #{new_resource.name}" do
+  execute "apm install #{new_resource.name}" do
     command   %{apm install '#{new_resource.name}'}
     not_if    %{apm list --bare | grep '^#{new_resource.name}'}
   end

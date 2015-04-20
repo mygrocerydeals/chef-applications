02e2a162917ac66cd600da2226282c6aafb97c2e
Daryl Robbins <darylrobbins@users.noreply.github.com>
2015-04-20 11:31:36 -0400

    Make atom package install optional

--- recipes/atom.rb
+++ recipes/atom.rb
@@ -2,6 +2,8 @@ include_recipe "homebrewalt::default"
 
 homebrewalt_cask "atom"
 
-node["atom_packages"].each do |package|
-    applications_atom_package package["name"]
+if node["atom_packages"] and node["atom_packages"].kind_of?(Array)
+    node["atom_packages"].each do |package|
+        applications_atom_package package["name"]
+    end
 end
